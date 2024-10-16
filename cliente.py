bdfedbd7043770694c8050257d0ee42493b0fdd0
angelocestari <angelocestarijunior10@gmail.com>
2024-10-16 14:14:28 -0400

    [FEAT] Validação campo id e username

--- cliente.py
+++ cliente.py
@@ -2,9 +2,26 @@
 
 from socket import *
 
+# Funções
+
+def limited_userId_input(prompt):
+    user_input = int(input(prompt))
+    while user_input < 1 or user_input > 999:
+        print(f'Entrada inválida! Intervalo de ids entre 1 e 999 ')
+        user_input = int(input(prompt))
+    return user_input
+
+def limited_username_input(prompt, max_length):
+    user_input = input(prompt).strip()
+    while len(user_input) > max_length or '' in user_input:
+        print(f"Entrada inválida! Máximo de {max_length} caracteres permitidos e sem espaços.")
+        user_input = input(prompt).strip()
+    return user_input
+
+
 # Código principal
-id = input('Id de usuário: ')
-nome_de_usuário = input('Nome de usuário: ')
+id = limited_userId_input("Digite seu id (Entre 1 e 999) ")
+nome_de_usuário = limited_username_input("Digite o seu nome de usuário (Max: 20 caracteres):", 20)
 ip = input('Endereço de IP: ')
 porta = input('Porta utilizada: ')
 timeout = 1
@@ -15,7 +32,8 @@ serverSocket.settimeout(timeout)
 
 porta = int(porta)
 
-data = f'0 {id} 0 0 {nome_de_usuário}'
+# Criando conexão
+data = f'0 {id} 0 0 {nome_de_usuário}' 
 
 try:
     serverSocket.sendto(data.encode(), (ip, porta))
