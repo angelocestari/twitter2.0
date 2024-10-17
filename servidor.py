8b2b90fc3e335726efc184f746802522965bf496
Francisco Ferreira Lima Neto <153586594+fflimaneto@users.noreply.github.com>
2024-10-16 21:57:50 -0400

    fix: respostas do servidor ajustadas; feat: novos testes para evitar erros

--- servidor.py
+++ servidor.py
@@ -17,19 +17,30 @@ def resposta_servidor(mensagem, endereco_cliente):
     
     #Particiona a mensagem para tratamento dos dados
     partes_mensagem = mensagem.split(' ')
+
+    if len(partes_mensagem) < 5:
+        #Retorna erro de mensagem inválida
+        texto = f"Mensagem sem a quantidade minima de campos"
+        mensagem = f'3 0 {partes_mensagem[1]} {len(texto)} servidor {texto}'
+        serverSocket.sendto(mensagem.encode(), endereco_cliente)
+
+        return
+
     contem_texto = 0
-    if len(partes_mensagem) > 5:
+    if len(partes_mensagem) > 5 and partes_mensagem[4] != '0':
         contem_texto = 1
 
     if partes_mensagem[0] == '0':
-        #Id disponível
-        if partes_mensagem[1] not in clientes_ativos and partes_mensagem[1] != '0':
+        if not (partes_mensagem[1] in clientes_ativos) and partes_mensagem[1] != '0':
+            #Id disponível, responde e salva
             serverSocket.sendto(mensagem.encode(), endereco_cliente)
             clientes_ativos[partes_mensagem[1]] = endereco_cliente
-        #Erro: Id indisponível
         else:
+            #Erro: Id indisponível, somente responde
             texto = f"Id {partes_mensagem[1]} indisponivel"
-            mensagem = f'3 0 {partes_mensagem[1]} {len(texto)} {texto}'
+            mensagem = f'3 0 {partes_mensagem[1]} {len(texto)} servidor {texto}'
+
+            serverSocket.sendto(mensagem.encode(), endereco_cliente)
 
     elif partes_mensagem[0] == '1':
         #Apaga registro do cliente
@@ -37,28 +48,34 @@ def resposta_servidor(mensagem, endereco_cliente):
 
     elif partes_mensagem[0] == '2':
         texto = ""
+        nome_origem = partes_mensagem[4] 
         if contem_texto:
-            texto = mensagem[5:]
+            texto = partes_mensagem[5:]
             texto = ' '.join(texto)
             texto = texto[:min(int(partes_mensagem[3]),140)]
         
-        clientes = {}
+        clientes_destino = {}
         if partes_mensagem[2] == '0':
-            clientes = clientes_ativos
+            #Envia para todos os clientes
+            clientes_destino = clientes_ativos
         else:
-            clientes[partes_mensagem[2]] = clientes_ativos[partes_mensagem[2]]
-        for id, cliente in clientes.items():
-            mensagem = f"2 {partes_mensagem[1]} {id} {len(texto)} {texto}"
+            #Enviar somente para um cliente especificado
+            clientes_destino[partes_mensagem[2]] = clientes_ativos[partes_mensagem[2]]
+
+        for id, cliente in clientes_destino.items():
+            mensagem = f"2 {partes_mensagem[1]} {id} {len(texto)} {nome_origem} {texto}"
             serverSocket.sendto(mensagem.encode(), cliente)
     else:
         texto = f"Mensagem enviada nao definida"
-        mensagem = f'3 0 {partes_mensagem[1]} {len(texto)} {texto}'     
+        mensagem = f'3 0 {partes_mensagem[1]} {len(texto)} servidor {texto}'
+
+    print(clientes_ativos)
 
 while True:
     #Espera receber uma mensagem
     mensagem, endereco_cliente = serverSocket.recvfrom(1024)
 
-    #responde a Mensagem
+    #responde a mensagem
     resposta_servidor(mensagem.decode(), endereco_cliente)
 
     
