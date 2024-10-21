ec7b0b5191f32a361c66c43290ef278f489dd7f2
angelocestari <angelocestarijunior10@gmail.com>
2024-10-20 20:58:48 -0400

    feat: falta apenas enviar/ler mensagem

--- cliente.py
+++ cliente.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from socket import *
+import threading
 
-
+# FUNÇÕES
 def limited_userId_input(user_input):
     try:
         user_id = int(user_input)
@@ -20,6 +21,30 @@ def limited_username_input(user_input):
         return None
     return user_input.strip()
 
+def escutar_mensagem():
+    try:
+        serverSocket = socket(AF_INET, SOCK_DGRAM)
+        serverSocket.bind(("",0))
+
+        while True:
+            message, _ = serverSocket.recvfrom(1024)
+            mensagem_decodificada = message.decode()
+
+            if 'mensagem_recebidas' not in st.session_state:
+                st.session_state['mensagens_recebidas'] = []
+            
+            st.session_state['mensagens_recebidas'].append(mensagem_decodificada)
+    except Exception as e:
+        st.error(f"Erro ao receber mensagem: {e}")
+
+if 'escutando_mensagens' not in st.session_state:
+    st.session_state['escutando_mensagens'] = False
+
+if not st.session_state['escutando_mensagens']:
+    threading.Thread(target=escutar_mensagem, daemon=True).start()
+    st.session_state['escutando_mensagens'] = True
+
+# PÁGINAS
 def pagina_criar_conexao():
     st.title("Insira os dados do cliente")
 
@@ -28,7 +53,10 @@ def pagina_criar_conexao():
 
     st.session_state['user_id'] = st.text_input("Digite seu ID (Entre 1 e 999)", value=st.session_state['user_id'])
 
-    username_input = st.text_input("Digite o seu nome de usuário (Máximo: 20 caracteres)")
+    if 'username_input' not in st.session_state:
+        st.session_state['username_input'] = ''
+
+    st.session_state['username_input'] = st.text_input("Digite o seu nome de usuário (Máximo: 20 caracteres)")
     
     if 'server_ip' not in st.session_state:
         st.session_state['server_ip'] = ''
@@ -38,87 +66,112 @@ def pagina_criar_conexao():
     st.session_state['server_ip'] = st.text_input("Endereço IP do servidor", value=st.session_state['server_ip'])
     st.session_state['server_port'] = st.text_input("Porta do servidor", value=st.session_state['server_port'])
 
-    if st.button('Enviar'):
+    if st.button('Criar conexão'):
 
         user_id = limited_userId_input(st.session_state['user_id'])
-        username = limited_username_input(username_input)
+        username = limited_username_input(st.session_state['username_input']) + '\0'
 
         if user_id and username and st.session_state['server_ip'] and st.session_state['server_port']:
             try:
                 serverSocket = socket(AF_INET, SOCK_DGRAM)
-                serverSocket.settimeout(1)
+                serverSocket.settimeout(5)
                 data = f'0 {user_id} 0 0 {username}'
                 serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
 
                 message, _ = serverSocket.recvfrom(1024)
                 st.success(f"Resposta do servidor: {message.decode()}")
-                serverSocket.close()
 
             except Exception as e:
                 st.error(f"Erro ao conectar ao servidor: {e}")
         else:
             st.error('Por favor, preencha todos os campos corretamente.')
 
-def pagina_enviar_mensagem():
-    st.title('ENVIAR MENSAGEM')
-
-    # if st.button("Listar Usuários Ativos"):
-    #     try:
-    #         serverSocket = socket(AF_INET, SOCK_DGRAM)
-    #         serverSocket.settimeout(1)
-    #         data = 'list'
-    #         serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
-
-    #         lista_usuarios,_ = serverSocket.recvfrom(1024)
-    #         st.write("Usuários Ativos:")
-    #         st.text(lista_usuarios.decode())
-    #         serverSocket.close()
-
-    #     except Exception as e:
-    #         st.error(f'Erro ao conectar ao servidor: {e}')
-
-    destino = st.text_input("ID de destino")
-    mensagem = st.text_input("Digite sua mensagem")
-
-    if st.button("Enviar Mensagem"):
-        try:
-            serverSocket = socket(AF_INET, SOCK_DGRAM)
-            serverSocket.settimeout(1)
-            tamanho_mensagem = len(mensagem)
-            data = f"2 {st.session_state['user_id']} {destino} {tamanho_mensagem} angelo {mensagem}"
-            serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
-
-            resposta, _ = serverSocket.recvfrom(1024)
-            st.write("Resposta do servidor ao enviar mensagem:")
-            st.text(resposta.decode())
-            serverSocket.close()
-        
-        except Exception as e:
-            st.error(f'Erro ao conectar ao servidor: {e}')
-
 def pagina_encerrar_conexao():
     st.title("Encerrar conexão")
 
-    try:
-        serverSocket = socket(AF_INET, SOCK_DGRAM)
-        serverSocket.settimeout(2)
-        if st.button("Encerrar conexão"):
-            data = f"1 {st.session_state['user_id']} 0 0 angelo"
+    if st.button('Encerrar conexão'):
+        username = st.session_state['username_input']
+        userId = st.session_state['user_id']
+        try:
+            serverSocket = socket(AF_INET, SOCK_DGRAM)
+            serverSocket.settimeout(5)
+            data = f'1 {userId} 0 0 {username}'
             serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
-            resposta,_ = serverSocket.recvfrom(1024)
-            st.text(resposta.decode())
-            serverSocket.close()
 
-    except Exception as e:
-        st.error(f"Erro ao encerrar conexão: {e}")
+            message, _ = serverSocket.recvfrom(1024)
+            st.success(f"Resposta do servidor: {message.decode()}")
+        except Exception as e:
+            st.error(f"Erro ao conectar ao servidor: {e}")
+    
+def pagina_enviar_mensagem():
+    st.title("Enviar mensagem")
+
+    if 'id_destino' not in st.session_state:
+        st.session_state['id_destino'] = ''
+
+    if 'mensagem' not in st.session_state:
+        st.session_state['mensagem'] = ''
+
+    st.session_state['id_destino'] = st.text_input("Digite ID do destino (Entre 1 e 999)", value=st.session_state['id_destino'])
+    st.session_state['mensagem'] = st.text_input("Digite a mensagem que deseja enviar", value=st.session_state['mensagem'])
+
+    if st.button('Enviar mensagem'):
+        try:
+            serverSocket = socket(AF_INET, SOCK_DGRAM)
+            serverSocket.settimeout(5)
+            data = f'2 {st.session_state['user_id']} {st.session_state['id_destino']} {len(st.session_state['mensagem'])} {st.session_state['username_input']} {st.session_state['mensagem']}'
+            serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
+            message, _ = serverSocket.recvfrom(1024)
+            st.success(f"Resposta do servidor: {message.decode()}")
+        except Exception as e:
+            st.error(f"Erro ao conectar ao servidor: {e}")
+
+def pagina_receber_mensagens():
+    st.title("Receber mensagens")
+
+    if 'mensagens_recebidas' not in st.session_state:
+        st.session_state['mensagens_recebidas'] = []
+    
+    st.subheader("Mensagens recebidas:")
+
+    if len(st.session_state['mensagens_recebidas']) == 0:
+        st.write("Nenhuma mensagem recebida.")
+    else:
+        for msg in st.session_state['mensagens_recebidas']:
+            st.write(msg)
+    
+
+def pagina_listar_clientes():
+    st.title("Solicitar lista de clientes conectados")
+
+    if st.button("Obter lista de clientes"):
+        try:
+            serverSocket = socket(AF_INET, SOCK_DGRAM)
+            serverSocket.settimeout(5)
+            
+            user_id = st.session_state['user_id']
+            data = f'4 {user_id} 0 0 servidor'
+            serverSocket.sendto(data.encode(), (st.session_state['server_ip'], int(st.session_state['server_port'])))
+
+            message, _ = serverSocket.recvfrom(1024)
+            mensagem_decodificada = message.decode()
+
+            st.success(f"Resposta do servidor: {mensagem_decodificada}")
+
+        except Exception as e:
+            st.error(f"Erro ao conectar ao servidor: {e}")
+        finally:
+            serverSocket.close()
 
 def mudar_pagina(pagina):
     st.session_state['pagina'] = pagina
 
 paginas = {
     "Criar Conexão": pagina_criar_conexao,
+    "Encerrar Conexão": pagina_encerrar_conexao,
     "Enviar Mensagem": pagina_enviar_mensagem,
-    "Encerrar Conexão": pagina_encerrar_conexao
+    "Receber Mensagens": pagina_receber_mensagens,
+    "Solicitar Lista de Clientes": pagina_listar_clientes
 }
 
 if 'pagina' not in st.session_state:
